79a6da5581731cb75116218e18d6b09e8139baf4
magedsopihy <magedsopihy@gmail.com>
2021-09-28 18:52:51 +0200

    refactor to microservice

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
+export POSTGRES_USERNAME=sopihydev
+export POSTGRES_PASSWORD=sopihydev
+export POSTGRES_HOST=postgress.cposnpe0q3kh.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=sopihy-microservices
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
 export JWT_SECRET=testing
-export URL=http://localhost:8100
+export URL=http://localhost:8080
